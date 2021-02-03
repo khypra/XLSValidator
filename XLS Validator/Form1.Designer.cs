66380108a6c12af57961b7bd811005a1697e98c0
Matheus <khypra007@gmail.com>
2021-02-03 10:26:29 -0300

    Segunda Versão
    adicionada funcionalidade de processamento das notas de um dia de cada filial para encontrar as diferenças, adicionada funcionalidade para acumular varios dias de processamento de nota antes de gerar a saida.

--- XLS Validator/Form1.Designer.cs
+++ XLS Validator/Form1.Designer.cs
@@ -31,15 +31,29 @@ namespace AtosCapital
         {
             this.button1 = new System.Windows.Forms.Button();
             this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(126, 140);
+            this.button1.Location = new System.Drawing.Point(109, 86);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Obter XLS";
+            this.button1.Text = "Obter XLS\'s";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.ObterXLS);
             // 
@@ -47,14 +61,126 @@ namespace AtosCapital
             // 
             this.openFile.FileName = "xlsFile";
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(9, 57);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(271, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Progresso";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 11);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(304, 152);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(296, 126);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Validar XLS\'s";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.dateTimePicker1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.progressBar2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(296, 126);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Listar Notas Faltando";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 21);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Gerar Relatório";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.GerarRelatorio);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(269, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Selecione a data Desejada antes de escolher o arquivo";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(9, 29);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(228, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Progresso";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Selecionar XLS";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CompararDiaErro);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(9, 70);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(271, 23);
+            this.progressBar2.TabIndex = 4;
+            // 
             // XLSValidator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(328, 175);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
             this.Name = "XLSValidator";
             this.Text = "XLS Validator";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -63,6 +189,17 @@ namespace AtosCapital
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- XLS Validator/Nota.cs
+++ XLS Validator/Nota.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace AtosCapital
 {
-    class Nota
+    public class Nota
     {
         public string nmArquivo { get; set; }
         public string dtNota { get; set; }
         public float vlMercadoria { get; set; }
         public float vlContabil { get; set; }
-
+        public string cdChave { get; set; }
         public float vlAtos { get; set; }
         public string descricao { get; set; }
 
@@ -21,19 +21,34 @@ namespace AtosCapital
 
             List<Nota> notas = new List<Nota>();
 
-            notas = notaNaoAgrupadas.GroupBy(e => new { e.nmArquivo, e.dtNota })
+            notas = notaNaoAgrupadas.GroupBy(e => new { e.nmArquivo, e.dtNota})
                 .Select(s => new Nota
-
                 {
                     dtNota = s.Key.dtNota,
                     nmArquivo = s.Key.nmArquivo,
                     vlMercadoria = s.Sum(a => a.vlMercadoria),
                     vlContabil = s.Sum(a => a.vlContabil),
-                }
-                ).ToList();
+                }).ToList();
 
             return notas;
 
+        }
+
+        public static List<Nota> notasAgrupadasPorChave(List<Nota> notasNaoAgrupadas, DateTime data)
+        {
+            List<Nota> notas = new List<Nota>();
+            notas = notasNaoAgrupadas.FindAll(n => n.dtNota == data.ToString("yyyyMMdd"));
+            notas = notas.GroupBy(g => g.cdChave)
+                .Select(s => new Nota
+                {
+                    cdChave = s.Key,
+                    dtNota = s.First().dtNota,
+                    nmArquivo = s.First().nmArquivo,
+                    vlMercadoria = s.Sum(a => a.vlMercadoria),
+                    vlContabil = s.Sum(a => a.vlContabil),
+                }).ToList();
+
+            return notas;
         }
 
     }
